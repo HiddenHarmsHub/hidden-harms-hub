a8d795f0ae97e1ec8fd1331d76dda3272ed2ed55
Cat Smith <910384+catsmith@users.noreply.github.com>
2025-11-19 16:51:58 +0000

    update the reporting tests to use colour schemes

--- jest_setup_files/accessibility_reporting_setup.js
+++ jest_setup_files/accessibility_reporting_setup.js
@@ -1,11 +1,13 @@
-/* global require, expect */
+/* global require, expect, beforeAll, afterAll */
 const fs = require('fs');
 const path = require('path');
 const pa11y = require('pa11y');
+const puppeteer = require('puppeteer');
 const htmlReporter = require('pa11y/lib/reporters/html');
 
+const chromeOptions = {"args": ["--no-sandbox"], "headless": "new"};
+
 const defaultOptions = {
-    "chromeLaunchConfig": {"args": ["--no-sandbox"]},
     "runners": ["axe", "htmlcs"],
     "includeWarnings": true,
     "includeNotices": true
@@ -13,8 +15,39 @@ const defaultOptions = {
 
 const reportFolder = "accessibility_reports";
 
-function writeReport(report, url) {
+let browser;
+
+beforeAll(async () => {
+    browser = await puppeteer.launch(chromeOptions);
+});
+
+afterAll(async () => {
+    await browser.close();
+});
+
+async function runPa11y(url, options, colourScheme, newBrowser) {
+    if (newBrowser) {
+        await browser.close();
+        browser = await puppeteer.launch(chromeOptions);
+    }
+    const page = await browser.newPage();
+    if (colourScheme !== undefined) {
+        await page.emulateMediaFeatures([
+            {name: 'prefers-color-scheme', value: colourScheme}
+        ]);
+    }
+    options.browser = browser;
+    options.page = page;
+    const results = await pa11y(url, options);
+    await page.close()
+    return results;
+}
+
+function writeReport(report, url, colourScheme) {
     let filename = url.replace(/\/$/, '').split('/').pop();
+    if (colourScheme !== undefined) {
+        filename = filename + '_' + colourScheme;
+    }
     let outputPath = path.join(reportFolder, filename + '.html');
     let i = 1;
     while (fs.existsSync(outputPath)) {
@@ -26,7 +59,7 @@ function writeReport(report, url) {
 }
 
 expect.extend({
-    async toBeAccessible (url, actions, waitTime) {
+    async toBeAccessible (url, actions, waitTime, colourScheme, newBrowser) {
         const options = defaultOptions;
         if (actions !== undefined) {
             options["actions"] = actions;
@@ -34,9 +67,9 @@ expect.extend({
         if (waitTime !== undefined) {
             options["wait"] = waitTime;
         }
-        const report = await pa11y(url, options);
+        const report = await runPa11y(url, options, colourScheme, newBrowser);
         const htmlReport = await htmlReporter.results(report);
-        writeReport(htmlReport, url);
+        writeReport(htmlReport, url, colourScheme);
         return {
             pass: true
         }
@@ -44,12 +77,12 @@ expect.extend({
 });
 
 expect.extend({
-    async allToBeAccessible(urls) {
+    async allToBeAccessible(urls, colourScheme) {
         let report, htmlReport;
         for (let i = 0; i < urls.length; i += 1) {
-            report = await pa11y(urls[i], defaultOptions);
+            report = await runPa11y(urls[i], defaultOptions, colourScheme);
             htmlReport = await htmlReporter.results(report);
-            writeReport(htmlReport, urls[i]);
+            writeReport(htmlReport, urls[i], colourScheme);
         }
         return {
             pass: true
@@ -59,9 +92,9 @@ expect.extend({
 });
 
 expect.extend({
-    async toHaveNoErrors(report, filename) {
+    async toHaveNoErrors(report, filename, colourScheme) {
         const htmlReport = await htmlReporter.results(report);
-        writeReport(htmlReport, filename);
+        writeReport(htmlReport, filename, colourScheme);
         return {
             pass: true
         }
