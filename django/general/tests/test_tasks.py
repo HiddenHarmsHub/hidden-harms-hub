a82755d3ad26d37fc0ebe967e99c45022f29cd9a
Cat Smith <910384+catsmith@users.noreply.github.com>
2026-05-26 10:39:07 +0100

    put in some proper data and fix broken test

--- django/general/tests/test_tasks.py
+++ django/general/tests/test_tasks.py
@@ -21,17 +21,31 @@ class TestMseTasks(TestCase):
         """Test that request is called with the right data and returns the result."""
         mock_response = MagicMock()
         mock_response.status_code = 200
-        mock_response.text = "a,b,c\n1,2,3\n"
+        response_text = """
+            parameter,estimate,ci_lower,ci_upper\n
+            alpha,5.248895,3.395394,6.6492987\n
+            beta_1,-1.9524562,-3.4493365,-0.68000245\n
+            beta_2,-1.6629716,-3.2102783,-0.37538695\n
+            beta_3,-2.355661,-3.8829818,-1.2214549\n
+            gamma_12,0.45588067,-1.0248151,1.4809868\n
+            gamma_13,0.9122592,-0.30105764,1.8032496\n
+            gamma_23,1.4185445,0.21890734,2.6326616\n
+        """
+        mock_response.text = response_text
         mock_post.return_value = mock_response
-        test_input = {"a": 1, "b": 2, "c": 3}
+        test_input = {
+            "list_data": [30, 40, 20, 10, 7, 18, 9],
+            "censoring_lower": 0,
+            "censoring_upper": 0,
+            "total_lists": 3,
+            "model_type": "NBE",
+        }
         task_result = calculate_mse(test_input)
         mock_post.assert_called_once_with(
-            self.test_url,
-            data=json.dumps(test_input),
-            headers={"Content-type": "application/json"},
-            timeout=200
+            self.test_url, data=json.dumps(test_input), headers={"Content-type": "application/json"}, timeout=200
         )
-        self.assertEqual(task_result, "a,b,c\n1,2,3\n")
+        self.assertEqual(task_result[0], response_text)
+        self.assertEqual(task_result[1], "NBE")
 
     @patch("general.tasks.requests.post")
     def test_calculate_mse_task_no_connection(self, mock_post):
